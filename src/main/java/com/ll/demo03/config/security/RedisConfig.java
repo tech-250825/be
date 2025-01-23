8def7c4583793b818c47cabe740475fd01ba08a1
Yoolim Han <ninetale22@gmail.com>
2025-01-24 00:09:09 +0900

    fix:redis

--- src/main/java/com/ll/demo03/config/security/RedisConfig.java
+++ src/main/java/com/ll/demo03/config/security/RedisConfig.java
@@ -4,21 +4,33 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
     @Value("${spring.redis.host}")
     private String redisHost;
+
     @Value("${spring.redis.port}")
     private int redisPort;
 
+    @Value("${spring.redis.password}")  // spring.data.redis -> spring.redis
+    private String redisPassword;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(redisHost);
+        redisConfig.setPort(redisPort);
+        redisConfig.setPassword(redisPassword); // 패스워드 설정
+
+        return new LettuceConnectionFactory(redisConfig);
     }
+
     @Bean
     public RedisTemplate<String, String> redisTemplate() {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
