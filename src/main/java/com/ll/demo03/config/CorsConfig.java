362620c10a44d5c682fd643a8aaf42e0c7e8cc74
Yoolim Han <ninetale22@gmail.com>
2025-03-30 18:19:17 +0900

    cors 설정 변경: 구체적인 도메인 설정

--- src/main/java/com/ll/demo03/config/CorsConfig.java
+++ src/main/java/com/ll/demo03/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
     @Primary
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*")); // 모든 도메인 허용
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:5713", "https://www.hoit.my")); // 모든 도메인 허용
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE")); // 모든 메소드 허용
         configuration.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
         configuration.setAllowCredentials(true); // 자격 증명 허용
