2b4b6e54f88c50c0dce1dbc7ebc2f1b6b65644d1
Yoolim Han <ninetale22@gmail.com>
2025-03-30 22:06:48 +0900

    도메인 추가

--- src/main/java/com/ll/demo03/config/security/OAuth2SuccessHandler.java
+++ src/main/java/com/ll/demo03/config/security/OAuth2SuccessHandler.java
@@ -50,7 +50,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
                 .path("/")
                 .maxAge(3600)
                 .sameSite("None")
-//                .domain("localhost.com")
+                .domain(".hoit.my")
                 .build();
         response.addHeader(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
 
@@ -61,7 +61,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
                 .path("/")
                 .maxAge(3600)
                 .sameSite("None")
-//                .domain("localhost.com")
+                .domain(".hoit.my")
                 .build();
         response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
 
