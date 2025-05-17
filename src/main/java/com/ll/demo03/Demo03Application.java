7c3bcc54ddfc4c89003ef52d0ad44c24499389a7
Yoolim Han <ninetale22@gmail.com>
2025-05-18 03:49:33 +0900

    deploy test

--- src/main/java/com/ll/demo03/Demo03Application.java
+++ src/main/java/com/ll/demo03/Demo03Application.java
@@ -1,6 +1,5 @@
 package com.ll.demo03;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
