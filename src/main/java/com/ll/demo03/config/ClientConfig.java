174e94bcd2efef8c887943bbe8665e5bbfb51fe8
kim400im <min2000jae@naver.com>
2025-07-13 15:54:58 +0900

    redo

--- src/main/java/com/ll/demo03/config/ClientConfig.java
+++ src/main/java/com/ll/demo03/config/ClientConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ClientConfig {
-
+    //dd
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
